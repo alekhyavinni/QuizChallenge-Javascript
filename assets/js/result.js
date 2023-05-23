e63d54261d3cc4cb87e7a06d6ade13621024aab6
bootcamp vinni <alekhyavinni12@gmail.com>
2023-05-23 14:38:50 -0400

    added new files

---
+++ assets/js/result.js
@@ -0,0 +1,18 @@
+var highscore=document.querySelector("#heading");
+var heading=document.querySelector("#score");
+var reset = document.querySelector("#goback");
+var clearbutton = document.querySelector("#clearscore");
+
+function getscore(){
+highscore.textContent="HIGHSCORE!";
+var playername=localStorage.getItem("INname");
+heading.textContent=(JSON.parse(playername).IN) + countvalue;
+reset.innerHTML="Go back";
+clearbutton.innerHTML="clear Highscores";
+
+
+reset.addEventListener('click',function quiz(){
+    window.location.href="quiz.html";
+});
+
+}
